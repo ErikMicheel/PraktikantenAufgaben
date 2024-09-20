784a06a3dbd9faeca17db8e30d14d1d479a76d9a
Erik Micheel <erik.micheel@ccd-mail.de>
2024-09-20 11:32:31 +0200

    added launch.json and optimize list_sorted.test.js

--- list_sort.test.js
+++ list_sort.test.js
@@ -1,7 +1,7 @@
 import { expect, test } from "bun:test";
 import {sort_arr} from  "./list_sort.js"
 
+var arr = Array.from({length:10}, () => Math.floor(Math.random() * 100));
 test("Teste Sortiert Funktion", () => {
-    var arr = Array.from({length:10}, () => Math.floor(Math.random() * 100));
     expect(sort_arr(arr)).toEqual(arr.sort())
 })
